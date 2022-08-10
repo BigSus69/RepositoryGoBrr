b6843dfa40d2dfb64356f7445aaf1939e192c219
Sune <sune171205@gmail.com>
2022-08-10 11:36:16 +0200

    Among us spil
    Meget simpelt, med masser af Jespers kode

---
+++ C# tutorial/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+//change appearance of console
+Console.Title = "Matrix";
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WindowHeight = 40;
+
+//Presentation
+Console.WriteLine("Hello Nerd!");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("You wanna do my homework?");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("If you don't, I'll call Microsoft support on you!");
+System.Threading.Thread.Sleep(1000);
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine("--------------------------------------------------------------");
+System.Threading.Thread.Sleep(500);
+Console.WriteLine("Transferring you to IndianTechSupport.dk");
+System.Threading.Thread.Sleep(2000);
+Console.WriteLine("...");
+System.Threading.Thread.Sleep(2000);
+Console.WriteLine("...");
+System.Threading.Thread.Sleep(500);
+Console.WriteLine("Transfer Succesful!");
+Console.WriteLine("--------------------------------------------------------------");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("Helo!");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("Microsoft support here :D");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("We have murdered your firstborn child!");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("LOL");
+Console.ForegroundColor = ConsoleColor.White;
+string answer = Console.ReadLine();
+static void DelayText(string text, int delay = 1000)
+{
+if(Console.ReadLine() == "y?")
+ System.Threading.Thread.Sleep(6900);
+   Console.WriteLine("Just cause lol");
+}
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Do my homework");
+Console.ReadKey();
